fe0d261ef3d729916b27030abed79fbc2736825f
ruslanashaari <ruslan@conversant.com.my>
2022-03-24 15:24:39 +0800

    feat: add employee resource

---
+++ app/Http/Resources/EmployeeResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\AddressResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmployeeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'code' => $this->code,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'created_at' => $this->created_at->diffForHumans(),
+            'deleted_at' => $this->deleted_at,
+            'address'   =>  $this->address ? new AddressResource($this->address) : ''
+        ];
+    }
+}
