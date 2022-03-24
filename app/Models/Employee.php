2973455943104fc4ea3d43eed79234aa66c2efcc
ruslanashaari <ruslan@conversant.com.my>
2022-03-24 15:37:29 +0800

    feat: implement soft delete traits in the employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Address;
 use App\Models\EmployeeRole;
 
 class Employee extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['code', 'first_name', 'last_name', 'address_id', 'employee_role_id'];
     protected $appends = ['full_name'];
