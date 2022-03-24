4fa63feb6d4631470553c948998e45a892f695bb
ruslanashaari <ruslan@conversant.com.my>
2022-03-24 15:16:30 +0800

    feat: add address factory

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'address_1'     => $this->faker->buildingNumber(),
+            'address_2'     => $this->faker->streetAddress(),
+            'district'      => $this->faker->city(),
+            'postcode'      => $this->faker->postcode(),
+            'city'          => $this->faker->state(),
+            'country_id'    => rand(1, 10),
+        ];
+    }
+}
