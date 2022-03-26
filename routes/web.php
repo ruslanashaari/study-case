9f3b7594839a0c007f95e2d2c6a13a2d88cf5ad6
ruslanashaari <ruslan@conversant.com.my>
2022-03-27 05:42:37 +0800

    fix: missing dir for address controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\AddressController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
