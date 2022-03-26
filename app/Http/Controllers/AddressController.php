a569087314791d63c49e7507176a4b4b6fea5557
ruslanashaari <ruslan@conversant.com.my>
2022-03-27 05:01:24 +0800

    feat: add AddressController and index

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\Request;
+use App\Models\Employee;
+use App\Models\Address;
+use Inertia\Inertia;
+use DB;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+
+            $addresses = AddressNameResource::collection(Address::all());
+            
+        } catch (Exception $e) {
+            return inertia('Addresses/Index', 
+                [
+                    $addresses => []
+                ])->withErrors($e->getMessage());
+        }
+
+        return inertia('Addresses/Index', compact('addresses'));
+    }
+}
