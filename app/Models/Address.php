22be1d760444b67a61832069250c38a79066de20
ruslanashaari <ruslan@conversant.com.my>
2022-03-27 12:02:18 +0800

    feat: implement soft delete model Address

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $appends = ['full_address'];
     protected $guarded = ['id'];
