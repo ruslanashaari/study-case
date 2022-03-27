3670f6801afa8e8832dd95b8cc7797b88a9ffd13
ruslanashaari <ruslan@conversant.com.my>
2022-03-27 11:14:46 +0800

    feat: add store method address controller

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Addresses\UpdateAddressAction;
+use App\Actions\Addresses\StoreAddressAction;
 use App\Http\Resources\AddressNameResource;
+use App\Http\Requests\UpdateAddressRequest;
+use App\Http\Requests\StoreAddressRequest;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Http\Request;
 use App\Models\Employee;
@@ -50,5 +54,30 @@ class AddressController extends Controller
                         ->withErrors($e->getMessage());            
         }
 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAddressRequest $request, StoreAddressAction $action)
+    {
+        try {
+            DB::beginTransaction();
+
+            $address = $action->handle($request->validated());   
+
+            DB::commit();
+
+        } catch (Exception $e) {
+            return redirect()
+                        ->route('addresses.create')
+                        ->withInput()
+                        ->withErrors($e->getMessage());      
+        }
+
+        return redirect()->route('addresses.show', $address->id)->with('success', 'Address created.');
     }
 }
